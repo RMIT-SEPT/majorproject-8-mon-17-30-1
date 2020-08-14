4c68d22c38f761eb594c55979ce4a113d1af7d9f
Lachlan Furlong <s3722243@student.rmit.edu.au>
2020-08-14 15:33:00 +1000

    Created basic pages

---
+++ FrontEnd/sept-react-frontend/src/component/about.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+export default class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header className="jumbotron">
+          <h3>About us</h3>
+        </header>
+      </div>
+    );
+  }
+}
