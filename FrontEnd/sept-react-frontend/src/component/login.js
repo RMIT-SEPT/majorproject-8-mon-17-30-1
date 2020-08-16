b84392a61cf44f418102ac457998cb32ca1f177a
Dean Bolte <s3784365@student.rmit.edu.au>
2020-08-16 16:11:29 +1000

    login template added

--- FrontEnd/sept-react-frontend/src/component/login.js
+++ FrontEnd/sept-react-frontend/src/component/login.js
@@ -11,6 +11,17 @@ export default class Login extends Component {
       <div className="container">
         <header className="jumbotron">
           <h3>Login</h3>
+          <form action="check_user.php" method="post">
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="text" class="form-control" name="email" placeholder="Enter your registered Email"></input>
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" class="form-control" name="password" placeholder="Enter your Password"></input>
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+          </form>
         </header>
       </div>
     );
