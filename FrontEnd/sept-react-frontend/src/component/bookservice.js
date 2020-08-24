b84771bf0953a7649bfbb4b5483c42363eb168e7
Dean Bolte <s3784365@student.rmit.edu.au>
2020-08-24 15:02:54 +1000

    added basic bookservice component

---
+++ FrontEnd/sept-react-frontend/src/component/bookservice.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class BookService extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header className="jumbotron">
+          <h3>Book a Service.</h3>
+
+        </header>
+      </div>
+    );
+  }
+}
