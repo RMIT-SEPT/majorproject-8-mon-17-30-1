c78e5bb6527980074389efafdd578a0623c1ebc9
Dean Bolte <s3784365@student.rmit.edu.au>
2020-09-02 19:29:43 +1000

    basic html for booking service

--- FrontEnd/sept-react-frontend/src/component/bookservice.js
+++ FrontEnd/sept-react-frontend/src/component/bookservice.js
@@ -9,9 +9,23 @@ export default class BookService extends Component {
     return (
       <div className="container">
         <header className="jumbotron">
-          <h3>Book a Service.</h3>
+            <h3>Book a Service.</h3>
 
+            <h4>Select an Available Service</h4>
+            <select name="ServiceSelect" id="ServiceSelect">
+                <option value="placeHolder">Place Holder 1</option>
+                <option value="placeHolder">Place Holder 2</option>
+                <option value="placeHolder">Place Holder 3</option>
+            </select>
+
+            <h4>Select an Available Time</h4>
+            <select name="TimeSelect" id="TimeSelect">
+                <option value="placeHolder">Place Holder 1</option>
+                <option value="placeHolder">Place Holder 2</option>
+                <option value="placeHolder">Place Holder 3</option>
+            </select>
         </header>
+
       </div>
     );
   }
