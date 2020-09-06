eaa3d4b243f02b8475aea6efa1b2ca6bf7015530
Dean Bolte <s3784365@student.rmit.edu.au>
2020-09-06 17:30:39 +1000

    added missing package.json and basic react for workerlist

---
+++ FrontEnd/sept-react-frontend/src/component/workerlist.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import Select from "react-validation/build/select";
+import DatePicker from "react-datepicker";
+
+export default class WorkerList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <p>WorkerList</p>
+        );
+    }
+}
