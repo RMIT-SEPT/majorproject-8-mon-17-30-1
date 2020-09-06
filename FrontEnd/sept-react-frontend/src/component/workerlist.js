b78dd09f12e4b9b74043f28d8cefe335cc55f69c
Dean Bolte <s3784365@student.rmit.edu.au>
2020-09-06 19:40:55 +1000

    fixed crash issue

--- FrontEnd/sept-react-frontend/src/component/workerlist.js
+++ FrontEnd/sept-react-frontend/src/component/workerlist.js
@@ -20,23 +20,20 @@ export default class WorkerList extends Component {
         super(props);
 
         this.onChangeWorker = this.onChangeWorker.bind(this);
-
-        this.state = {
-            worker: ""
-        };
     }
 
-    onChangeWorker(e) {
-        this.setState({
-            worker: e.target.value
-        });
+    state = {
+        selected: ""
+    };
+
+    onChangeWorker = (selected) => {
+        this.setState({ selected });
     }
 
     render() {
         return (
             <Select name='worker'
                     options={workerOptions}
-                    value={this.state.worker}
                     onChange={this.onChangeWorker}
                     validations={[required]}
             />
