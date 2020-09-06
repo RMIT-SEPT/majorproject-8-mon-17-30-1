edfc0e5f5b5dcd3c89759f45a90a13df1b5fc1ed
Dean Bolte <s3784365@student.rmit.edu.au>
2020-09-06 17:03:08 +1000

    added react for date list comp

--- FrontEnd/sept-react-frontend/src/component/datelist.js
+++ FrontEnd/sept-react-frontend/src/component/datelist.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
     }
 
+    state = {
+        startDate: new Date()
+    };
+
+    handleDateChange = date => {
+        this.setState({
+            startDate: date
+        });
+    };
+
     render() {
         return (
-            <div className="container">
-                <h4></h4>
-                <select>
-                </select>
-            </div>
+            <DatePicker
+                selected={this.state.startDate}
+                onChange={this.handleDateChange}
+                showTimeSelect
+                dateFormat="Pp"
+            />
         );
     }
 }
