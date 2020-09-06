edaec849d05006346bb0659e13c5d0ec72616dec
Dean Bolte <s3784365@student.rmit.edu.au>
2020-09-06 18:17:43 +1000

    added broken base react

--- FrontEnd/sept-react-frontend/src/component/workerlist.js
+++ FrontEnd/sept-react-frontend/src/component/workerlist.js
@@ -1,15 +1,45 @@
 import React, {Component} from "react";
-import Select from "react-validation/build/select";
-import DatePicker from "react-datepicker";
+import Select from "react-select";
+
+const workerOptions = [
+    { value: 'template', label: 'Template' }
+]
+
+const required = value => {
+    if (!value) {
+        return (
+            <div className="alert alert-danger" role="alert">
+                This field is required!
+            </div>
+        );
+    }
+};
 
 export default class WorkerList extends Component {
     constructor(props) {
         super(props);
+
+        this.onChangeWorker = this.onChangeWorker.bind(this);
+
+        this.state = {
+            worker: ""
+        };
+    }
+
+    onChangeWorker(e) {
+        this.setState({
+            worker: e.target.value
+        });
     }
 
     render() {
         return (
-            <p>WorkerList</p>
+            <Select name='worker'
+                    options={workerOptions}
+                    value={this.state.worker}
+                    onChange={this.onChangeWorker}
+                    validations={[required]}
+            />
         );
     }
 }
