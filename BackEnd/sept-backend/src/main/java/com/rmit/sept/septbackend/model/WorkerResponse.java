e276130a45f8d6e35df811cd8dbab9706c09d95b
Lachlan Furlong <s3722243@student.rmit.edu.au>
2020-09-07 13:10:41 +1000

    Added get workers

---
+++ BackEnd/sept-backend/src/main/java/com/rmit/sept/septbackend/model/WorkerResponse.java
@@ -0,0 +1,18 @@
+package com.rmit.sept.septbackend.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@JsonNaming(PropertyNamingStrategy.KebabCaseStrategy.class)
+public class WorkerResponse {
+    private int workerId;
+    private String firstName;
+    private String lastName;
+}
--- BackEnd/sept-backend/src/main/java/com/rmit/sept/septbackend/repository/WorkerRepository.java
+++ BackEnd/sept-backend/src/main/java/com/rmit/sept/septbackend/repository/WorkerRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface WorkerRepository extends CrudRepository<WorkerEntity, Integer> {
     boolean existsByUserUsername(String username);
+
+
 }
