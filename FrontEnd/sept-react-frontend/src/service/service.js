c5a5ef10ec764f9c357b56771c3c41513b2e5296
Dean Bolte <s3784365@student.rmit.edu.au>
2020-09-07 13:10:35 +1000

    added axios functionality

--- FrontEnd/sept-react-frontend/src/service/service.js
+++ FrontEnd/sept-react-frontend/src/service/service.js
@@ -4,6 +4,8 @@ import authHeader from './auth-header';
 // For local development, this is hardcoded in at the moment
 const SERVICE_URL = "http://localhost:8080/api/v1/service/";
 const BOOKING_URL = "http://localhost:8080/api/v1/booking/";
+const BUSINESS_URL = "http://localhost:8080/api/v1/business/";
+const WORKER_URL = "http://localhost:8080/api/v1/worker/";
 
 class Service {
     bookService(serviceID, workerID, dateTime, username) {
@@ -13,20 +15,28 @@ class Service {
                 workerID,
                 dateTime,
                 username
-            }, {headers: authHeader()})
+            }, {headers: authHeader()}).then(response => {
+                return response.data;
+            });
     }
 
     // Probably an incorrect place to put this
     getBusinessesAll() {
-        return axios.get();
+        return axios.get(BUSINESS_URL, {headers: authHeader()}).then(response => {
+            return response.data;
+        });
     }
 
     getServicesByBusinessID(businessID) {
-        return axios.get(SERVICE_URL, businessID);
+        return axios.get(SERVICE_URL, {params: {businessID}, headers: authHeader()}).then(response => {
+            return response.data;
+        });
     }
 
     getWorkersByServiceID(serviceID) {
-
+        return axios.get(WORKER_URL, {params: {serviceID}, headers: authHeader()}).then(response => {
+            return response.data;
+        });
     }
 
 }
