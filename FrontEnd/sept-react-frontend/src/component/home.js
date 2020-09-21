b19aec0a0a1c19a3c87d19aff7971c1c4f7e947a
Dean Bolte <s3784365@student.rmit.edu.au>
2020-09-21 13:18:29 +1000

    removed useless comments, changed visuals

--- FrontEnd/sept-react-frontend/src/component/home.js
+++ FrontEnd/sept-react-frontend/src/component/home.js
@@ -1,41 +1,16 @@
 import React, { Component } from "react";
 
-
-
-// import UserService from "../services/user.service";
-
 export default class Home extends Component {
   constructor(props) {
     super(props);
-
-    // this.state = {
-    //   content: ""
-    // };
   }
-
-//   componentDidMount() {
-//     UserService.getPublicContent().then(
-//       response => {
-//         this.setState({
-//           content: response.data
-//         });
-//       },
-//       error => {
-//         this.setState({
-//           content:
-//             (error.response && error.response.data) ||
-//             error.message ||
-//             error.toString()
-//         });
-//       }
-//     );
-//   }
 
   render() {
     return (
       <div className="container">
         <header className="jumbotron">
-          <h3>Welcome dean</h3>
+          <h3>Welcome to AGME</h3>
+          <p>Please sign in to view the Dashboard</p>
         </header>
       </div>
     );
